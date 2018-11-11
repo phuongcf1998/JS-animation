c3a1b084cf16de71723704407bb93fcfd972eeff
Phuong Nguyễn <44605994+phuongcf1998@users.noreply.github.com>
2018-11-11 14:58:25 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,33 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	var nut=document.getElementsByClassName('declick');
+	var nd=document.getElementsByClassName('dehienthi');
+	for (var i = 0; i < nut.length; i++) {
+		nut[i].onclick=function(){
+
+
+			if(this.classList[1]=='mautrang'){
+				this.classList.remove('mautrang');
+				var ndhienra=this.getAttribute('data-hienlen');
+				var phantuHienra=document.getElementById(ndhienra);
+				phantuHienra.classList.remove('ra');
+			}
+			else{
+				for (var k = 0; k < nut.length; k++) {
+					nut[k].classList.remove('mautrang')
+				}
+				this.classList.toggle('mautrang');
+				var ndhienra=this.getAttribute('data-hienlen');
+				var phantuHienra=document.getElementById(ndhienra);
+				for (var i = 0; i < nd.length; i++) {
+					nd[i].classList.remove('ra');
+				}
+				phantuHienra.classList.toggle('ra');
+
+			}
+			
+
+
+		}
+	}
+
+},false)
